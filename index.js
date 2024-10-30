352fd7b86b7e5ca857dd9cf9868899bf9b744f25
Md Zakir Hossain <58882834+Zakir-1995@users.noreply.github.com>
2024-10-30 13:00:58 +0600

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors());
 
 
 // connect DB

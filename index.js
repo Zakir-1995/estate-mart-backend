caff038e6dfaff92129a44fc1b89c022172ea881
Md Zakir Hossain <58882834+Zakir-1995@users.noreply.github.com>
2024-10-30 13:55:51 +0600

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors({credentials: true}));
+app.use(cors({credentials: false}));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');

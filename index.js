37bbc4c91ba4ad5aa48b7b284859581793a9cc22
Md Zakir Hossain <58882834+Zakir-1995@users.noreply.github.com>
2024-10-30 15:14:56 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
-import cors from "cors";
+
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
@@ -12,10 +12,10 @@ const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors());
+
 
 app.use((req, res, next) => {
-res.header('Access-Control-Allow-Origin', '*');
+res.header('Access-Control-Allow-Origin',"https://estate-mart.netlify.app/");
  next();
  });
 // connect DB

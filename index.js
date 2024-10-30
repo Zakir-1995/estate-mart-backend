ded809e11729e7e2b1d656b4065613a85ecdfc46
Md Zakir Hossain <58882834+Zakir-1995@users.noreply.github.com>
2024-10-30 15:46:44 +0600

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(cookieParser());
 
 app.use((req, res, next) => {
 res.header('Access-Control-Allow-Origin',"https://estate-mart.netlify.app/");
+ res.header('Access-Control-Allow-Credentials',"true");
  next();
  });
 // connect DB

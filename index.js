a7e8383ad3c5b16c8db5820388169d236df06dc5
Md Zakir Hossain <58882834+Zakir-1995@users.noreply.github.com>
2024-10-30 13:29:03 +0600

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(cors());
 
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 // connect DB
 mongoose
   .connect(process.env.MONGO_URI)

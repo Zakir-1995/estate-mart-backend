0ed75c9d9c182d8f81c867c3324922510db3f16f
Md Zakir Hossain <58882834+Zakir-1995@users.noreply.github.com>
2024-10-30 20:48:41 +0600

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ app.use(cookieParser());
 // app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 
-app.use((req, res, next) => {
-res.header('Access-Control-Allow-Origin',"*");
- next();
- });
+// app.use((req, res, next) => {
+// res.header('Access-Control-Allow-Origin',"*");
+//  next();
+//  });
 // connect DB
 mongoose
   .connect(process.env.MONGO_URI)
